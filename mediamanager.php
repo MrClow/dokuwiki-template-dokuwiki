c65fe355ae04596ab5cf07072dce17d25c35c5fa
Anika Henke <anika@selfthinker.org>
2010-10-24 15:35:31 +0100

    overhauled rest of styles

--- mediamanager.php
+++ mediamanager.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
 /**
  * DokuWiki Default Template
@@ -12,33 +10,35 @@
  * @link   http://dokuwiki.org/templates
  * @author Andreas Gohr <andi@splitbrain.org>
  */
-?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>" lang="<?php echo $conf['lang']?>" dir="ltr">
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>"
+  lang="<?php echo $conf['lang']?>" dir="<?php echo $lang['direction'] ?>" class="popup">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>
-    <?php echo hsc($lang['mediaselect'])?>
-    [<?php echo strip_tags($conf['title'])?>]
-  </title>
-  <?php tpl_metaheaders()?>
-  <link rel="shortcut icon" href="<?php echo DOKU_TPL?>images/favicon.ico" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>
+        <?php echo hsc($lang['mediaselect'])?>
+        [<?php echo strip_tags($conf['title'])?>]
+    </title>
+    <?php tpl_metaheaders()?>
+    <link rel="shortcut icon" href="<?php echo ml('favicon.ico') ?>" />
 </head>
 
 <body>
-<div id="media__manager" class="dokuwiki">
-    <div id="media__left">
-        <?php html_msgarea()?>
-        <h1><?php echo hsc($lang['mediaselect'])?></h1>
+    <div id="media__manager" class="dokuwiki">
+        <?php html_msgarea() ?>
+        <div id="media__left"><div class="pad">
+            <h1><?php echo hsc($lang['mediaselect'])?></h1>
 
-        <?php /* keep the id! additional elements are inserted via JS here */?>
-        <div id="media__opts"></div>
+            <?php /* keep the id! additional elements are inserted via JS here */?>
+            <div id="media__opts"></div>
 
-        <?php tpl_mediaTree() ?>
+            <?php tpl_mediaTree() ?>
+        </div></div>
+
+        <div id="media__right"><div class="pad">
+            <?php tpl_mediaContent() ?>
+        </div></div>
     </div>
-
-    <div id="media__right">
-        <?php tpl_mediaContent() ?>
-    </div>
-</div>
 </body>
 </html>
